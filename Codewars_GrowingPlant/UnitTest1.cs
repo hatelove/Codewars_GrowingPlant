a2dd1f827e9e0aad007abdfb6ec66194adc57e15
hatelove <jay.hatelove@gmail.com>
2017-03-20 22:35:07 +0800

    add the first failed test case: first day, upSpeed fulfill desiredHeigth

---
+++ Codewars_GrowingPlant/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Codewars_GrowingPlant
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void firstDay_fulfill()
+        {
+            Assert.AreEqual(1, new Kata().GrowingPlant(10, 9, 4));
+        }
+    }
+
+    public class Kata
+    {
+        public int GrowingPlant(int upSpeed, int downSpeed, int desiredHeight)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
